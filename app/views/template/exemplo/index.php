60e7f8aeab8e48ec77b1ce10fc7af13e50aee575
rafaelrgb <faerugb@gmail.com>
2018-08-08 12:26:44 -0300

    Componente blog-post

--- app/views/template/exemplo/index.php
+++ app/views/template/exemplo/index.php
@@ -1,42 +1,14 @@
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-            <a href="/">Home</a>
-        </li>
-        <li class="breadcrumb-item">
-            <a href="/docs">Docs</a>
-        </li>
-        <li class="breadcrumb-item active">Vue</li>
-    </ol>
-</nav>
-<section>
-    <div class="container-fluid">
-        <h3 class="text-center">Exemplo de uso do Vue</h3>
-        <hr>
-        <p class="text-center"><small>Criação de uma lista de thumbnails usando <code>v-for</code>.<br> Para mais informações, consulte a <a href="https://vuejs.org/v2/guide/#What-is-Vue-js" target="_blank">documentação</a> do Vue</small></p>
-    </div>
+<div id="exampleApp" class="posts-gallery">    
+    <div class="container">
+        <div class="row">
 
-    <div class="container-fluid">
-        <h3 class="text-center">Resultado </h3>
-        <hr>
-        <div id="exampleApp">
+            <div class="col-lg-4" v-for="post in posts">
 
-            <h3>{{trips.code}}</h3>
-            <p>{{trips.message}}</p>
-            <pre>{{trips.data}}</pre>
+                <blog-post v-bind:post="post" v-bind:key="post.id"></blog-post>    
 
-            <!-- <div class="col-md-3 col-sm-6 col-xs-12" v-for="trip in trips">
-                <div class="thumbnail">
-                    <img v-bind:src="trip.imgSrc" v-bind:alt="trip.imgAlt">
-                    <div class="caption">
-                        <h3>{{trip.title}}</h3>
-                        <p>{{trip.comment}}</p>
-                        <p>
-                            <button class="btn btn-danger" role="button" v-on:click="removeTrip(trip.id)">Remover</button>
-                        </p>
-                    </div>
-                </div>
-            </div> -->
+            </div>
+
         </div>
     </div>
-</section>
+</div>
+
--- assets/js/exemplo.js
+++ assets/js/exemplo.js
@@ -1,7 +1,20 @@
+Vue.component('blog-post', {
+    props: ['post'],
+    template: '<div class="blog-post">' +
+            '<img class="blog-post-img" v-bind:src="post.img" v-bind:alt="post.title">' +
+            '<div class="blog-post-body">' +
+                '<p><small class="blog-post-tag">{{post.tag}}</small></p>' +   
+                '<h5 class="blog-post-title">{{post.title}}</h5>' +
+                '<p class="blog-post-text">{{post.text}}</p>' +
+                '<a href="#" class="btn btn-primary">LER MAIS</a>' +
+            '</div>' +
+        '</div>'
+  })
+
 var vueExampleApp = new Vue({
     el: "#exampleApp",
     data: {
-        trips: {}
+        posts: []
     },
     methods: {
         removeTrip: function (tripId) {
@@ -15,8 +28,8 @@ var vueExampleApp = new Vue({
         }
     },
     created() { // vue lifecycle hook
-        $.get('/sfadsfsfas', null, null, 'json').then(response => {
-            this.trips = response;
+        $.get('/exemplo', null, null, 'json').then(response => {
+            this.posts = response.data.posts;
         });
     }
 });
