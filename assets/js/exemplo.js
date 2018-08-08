3230d416feea1eaee15b8601f2c90202e7bbb18d
rafaelrgb <faerugb@gmail.com>
2018-08-08 12:50:05 -0300

    Modificação do componente blog-post

--- assets/js/exemplo.js
+++ assets/js/exemplo.js
@@ -1,14 +1,16 @@
 Vue.component('blog-post', {
     props: ['post'],
-    template: '<div class="blog-post">' +
-            '<img class="blog-post-img" v-bind:src="post.img" v-bind:alt="post.title">' +
-            '<div class="blog-post-body">' +
-                '<p><small class="blog-post-tag">{{post.tag}}</small></p>' +   
-                '<h5 class="blog-post-title">{{post.title}}</h5>' +
-                '<p class="blog-post-text">{{post.text}}</p>' +
-                '<a href="#" class="btn btn-primary">LER MAIS</a>' +
-            '</div>' +
-        '</div>'
+    template: `
+        <div class="blog-post">
+            <img class="blog-post-img" v-bind:src="post.img" v-bind:alt="post.title">
+            <div class="blog-post-body">
+                <p><small class="blog-post-tag">{{post.tag}}</small></p>
+                <h5 class="blog-post-title">{{post.title}}</h5>
+                <p class="blog-post-text">{{post.text}}</p>
+                <a href="#" class="btn btn-primary">LER MAIS</a>
+            </div>
+        </div>
+    `
   })
 
 var vueExampleApp = new Vue({
